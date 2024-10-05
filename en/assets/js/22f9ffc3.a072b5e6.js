0c58ce4b6ed0b31e082abb8c6d2a68e754848a79
ajudafacity <ajudafacity@users.noreply.github.com>
2024-10-05 01:09:56 +0000

    deploy: 5208545a558c17686948c0b6dc0b910c079f57c5

--- en/assets/js/22f9ffc3.a072b5e6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkajudafacitysistemas=self.webpackChunkajudafacitysistemas||[]).push([[2782],{6004:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
